a95e4ddd2477aa267396bd4a00fed82329d11ee0
JordanMills233 <jordanmills233@gmail.com>
2021-12-03 09:30:33 +0000

    Add project files.

---
+++ Core/Issue.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Issue
+    {
+        public int IssueId { get; set; }
+        [Required]
+        public int? ProjectId { get; set; }
+        [Required]
+        public DateTime Timestamp { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int? SeverityId { get; set; }
+
+    }
+}
---
+++ Core/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        [Required]
+        public string? ProjectName { get; set; }
+
+        public string? ProjectDescription { get; set; }
+
+        //naviation property for ef core
+        public Issue ?Issue { get; set; }
+
+    }
+}
